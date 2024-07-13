6d265f9c49b149b6baf2f3222682d995b467054f
Gabriel Bruno <114627827+GabrielBruno7@users.noreply.github.com>
2024-07-13 10:14:49 -0300

    feat: update handler structure to separate subpackages

--- router/routes.go
+++ router/routes.go
@@ -9,26 +9,15 @@ func initializeRoutes(router *gin.Engine) {
 	v1 := router.Group("/api/v1")
 
 	{
-		v1.GET("/opening", func(context *gin.Context) {
-			handler.ShowOpeningHandler(context)
-		})
+		v1.GET("/opening", handler.ShowOpeningHandler)
 
-		v1.POST("/opening", func(context *gin.Context) {
-			handler.CreateOpeningHandler(context)
-		})
+		v1.POST("/opening", handler.CreateOpeningHandler)
 
-		v1.PUT("/opening", func(context *gin.Context) {
-			handler.UpdateOpeningHandler(context)
-		})
+		v1.PUT("/opening", handler.UpdateOpeningHandler)
 
-		v1.DELETE("/opening", func(context *gin.Context) {
-			handler.DestroyOpeningHandler(context)
-		})
+		v1.DELETE("/opening", handler.DestroyOpeningHandler)
 
-		v1.GET("/openings", func(context *gin.Context) {
-			handler.ListOpeningsHandler(context)
-		})
-
+		v1.GET("/openings", handler.ListOpeningsHandler)
 	}
 
 }
