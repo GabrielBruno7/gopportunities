39be40395007ff67688a2c6d582e1f528db3bf9f
Gabriel Bruno <114627827+GabrielBruno7@users.noreply.github.com>
2024-07-13 10:22:35 -0300

    extracting handlers to unified folders

--- handler/handler.go
+++ handler/handler.go
@@ -1,39 +1,10 @@
 package handler
 
-import (
-	"net/http"
-
-	"github.com/gin-gonic/gin"
-)
-
-func ShowOpeningHandler(context *gin.Context) {
-	context.JSON(http.StatusOK, gin.H{
-		"msg": "GET Opening",
-	})
-}
+func 
 
 
-func CreateOpeningHandler(context *gin.Context) {
-	context.JSON(http.StatusOK, gin.H{
-		"msg": "POST Opening",
-	})
-}
 
-func UpdateOpeningHandler(context *gin.Context) {
-	context.JSON(http.StatusOK, gin.H{
-		"msg": "PUT Opening",
-	})
-}
 
-func DestroyOpeningHandler(context *gin.Context) {
-	context.JSON(http.StatusOK, gin.H{
-		"msg": "DELETE Opening",
-	})
-}
 
-func ListOpeningsHandler(context *gin.Context) {
-	context.JSON(http.StatusOK, gin.H{
-		"msg": "GET Openings",
-	})
-}
+
 
